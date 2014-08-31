e40921a6527c4a7c7f0851cbcdd82ae0d9a569c4
Mike King <mikeking3@gmail.com>
2014-08-30 23:51:51 -0700

    incremental progress on footer behavior

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-//global list of items
+//COLLECTION
 var todos = [];
 
 //i want the div to toggle based on whether there are any todos 
 //or not.  not sure how to accomplish this with d3 yet
-todos.toggleMain = function(){
+todos.toggleVisibility = function(){
     var main = document.getElementById('main');
     var footer = document.getElementById('footer');
     var correctStyle = (this.length > 0)?'block':'none';
@@ -13,24 +13,13 @@ todos.toggleMain = function(){
     };
 };
 
-//adding a new item
-var newItemInput = d3.select('#new-todo');
+//MODEL
+function Todo(obj){
+    this.title = obj.title || '';
+    this.completed = obj.completed || false;
+};
 
-newItemInput.on('keypress', function(d, i){
-    if (d3.event.keyCode === 13){
-        //add new item
-        todos.push(new Todo({title: this.value}));
-
-        //prepare input field for next item
-        this.value = '';
-        
-        //show div#main if hidden
-        todos.toggleMain();
-
-        //render the view
-        renderTodos(todos);
-    };
-});
+//VIEW-RELATED
 
 function renderTodos(todos){
     //insert new item into DOM
@@ -39,6 +28,21 @@ function renderTodos(todos){
         .data(todos)
         .enter()
         .append(makeListItem);
+};
+
+function renderFooter(todos){
+    var $ = document.querySelector;
+    var $$ = document.querySelectorAll;
+    $('#todo-count strong').textContent = todos.length;
+    $('#todo-count span').textContent = (todos.length === 1)?'item':'items';
+
+    var hash = location.hash || '#';
+    var links = $$('#filters a');
+    links = Array.prototype.slice.call(links, 0);
+    links.forEach(function(link){
+        //highlight the right link.  router-like function.
+        //too sleepy right now.
+    });
 };
 
 //rendering a new item
@@ -54,6 +58,9 @@ function makeListItem(todo){
     var checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.classList.add('view');
+    if (todo.completed){
+        checkbox.checked = true;
+    };
 
     //label
     var label = document.createElement('label');
@@ -80,9 +87,26 @@ function makeListItem(todo){
     return li;
 };
 
-//class definition
-function Todo(obj){
-    this.title = obj.title || '';
-    this.completed = obj.completed || false;
-};
+//CONTROLLER
+
+//react to new item
+var newItemInput = d3.select('#new-todo');
+newItemInput.on('keypress', function(d, i){
+    if (d3.event.keyCode === 13){
+        //add new item
+        todos.push(new Todo({title: this.value}));
+
+        //prepare input field for next item
+        this.value = '';
+        
+        //show div#main if hidden
+        todos.toggleVisibility();
+
+        //render the view
+        renderTodos(todos);
+        renderFooter(todos);
+    };
+});
+
+
 
